5ec89a8d2d398623415935f5d8a454baa495c512
MalingaPeiris <63008840+MalingaPeiris@users.noreply.github.com>
2021-11-25 21:28:52 +0530

    nodeman

---
+++ backend/server.js
@@ -0,0 +1,12 @@
+const app = require("./app");
+
+const dotenv = require("dotenv");
+
+//setting up config file path
+dotenv.config({ path: "backend/config/config.env" });
+
+app.listen(process.env.PORT, () => {
+  console.log(
+    `Server started on PORT: ${process.env.PORT} in ${process.env.NODE_ENV} mode`
+  );
+});
